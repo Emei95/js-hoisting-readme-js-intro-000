690325e7fa6c98b2b3b345861c9dbe1811b8cb4e
Eric Mei <shizizerick@aol.com>
2018-06-05 16:29:14 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -11,13 +11,7 @@ function crazy(){
   crazy();
 
 function sayMyName() {
-  // fix the code in here:
-  var name = "Cricky";
-
-  sayMy();
-
-  function sayMy() {
-    console.log(name);
     var name = "Kristin";
+        console.log(name);
   }
-}
+sayMyName();
