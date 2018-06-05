28a2db94293bf6fe35c50c1fa780dd864491e637
Eric Mei <shizizerick@aol.com>
2018-06-05 15:36:45 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -1,12 +1,9 @@
-function callMe() {
-  console.log("I just met you...");
-  console.log("and this is crazy..");
-  console.log("but here's my number..");
-  console.log("so call me");
-  return lyric;
-  var lyric = "maybe";
-}
 
+function callMe() {
+var lyric = callMe();
+  console.log(maybe)
+}
+lyric();
 
 function crazy() {
   // fix the code in here:
